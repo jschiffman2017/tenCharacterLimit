37a739803e9651b7039bbd632e0ca82b86448349
Jack Schiffman <jschiffman2017@pingry.org>
2016-03-03 11:49:44 -0500

    Program working.

--- tenCharacterLimit.py
+++ tenCharacterLimit.py
@@ -3,8 +3,20 @@ lines = f.readlines()
 message = f.read()
 f.close()
 f = open("output.txt", "w")
+output = ""
 for line in lines:
+	index = 0
 	if len(line) <= 10:
-		f.write(line)
+		output = line
+		f.write(output)
+	else:
+		while index < 10:
+			output += line[index]
+			index += 1
+		f.write(output)
+		f.write("\n")
+	
+	output = ""
 
 f.close()
+exit()
