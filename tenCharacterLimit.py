89bf8f99829ce3283b24366e6ee9c7c779595c1d
Jack Schiffman <jschiffman2017@pingry.org>
2016-03-03 11:55:43 -0500

    Simplified program.

--- tenCharacterLimit.py
+++ tenCharacterLimit.py
@@ -1,10 +1,9 @@
 f = open("input.txt", 'r')
 lines = f.readlines()
-message = f.read()
 f.close()
 f = open("output.txt", "w")
-output = ""
 for line in lines:
+	output = ""
 	index = 0
 	if len(line) <= 10:
 		output = line
@@ -15,8 +14,6 @@ for line in lines:
 			index += 1
 		f.write(output)
 		f.write("\n")
-	
-	output = ""
 
 f.close()
 exit()
